26a7e391c1dc68d42ab3f62b84453792fb273b55
Asem Ashraf <73125888+Asem-Ashraf@users.noreply.github.com>
2022-09-01 16:32:45 +0200

    Delete assignments directory

--- assignments/ps0/ps0/PythonApplication1.py
+++
@@ -1,7 +0,0 @@
-
-import numpy
-import pylab
-x = float(input("Enter number x: "))
-y = float(input("Enter number y: "))
-print ("X**y= ",x**y)
-print ("log (x)= ", int(numpy.log2(x)))
--- assignments/ps0/ps0/ps0.py
+++
@@ -1,6 +0,0 @@
-import math
-
-x = float(input("Enter number x: "))
-y = float(input("Enter number y: "))
-print ("X**y= ",x**y)
-print ("log (x)= ",math.log2(x))
